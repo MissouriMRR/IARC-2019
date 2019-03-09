02b51038f6e962a235b9d6f35d8cea0c5cdf8897
christopher-o-toole <cdocq9@mst.edu>
2019-03-09 00:21:58 -0600

    Some OpenCV installtion script fixes.

--- flight/devices/scripts/install_opencv.sh
+++ flight/devices/scripts/install_opencv.sh
@@ -1,10 +1,10 @@
-sudo apt-get install build-essential cmake unzip pkg-config
-sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libgtk-3-dev
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install python3-dev python3-pip
+sudo apt-get install -y build-essential cmake unzip pkg-config
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y python3-dev python3-pip python3-numpy python3-dbg
 
 cd ~
 wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.4.zip
@@ -12,6 +12,8 @@ wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.
 
 unzip opencv.zip
 unzip opencv_contrib.zip
+sudo rm -rf opencv
+sudo rm -rf opencv_contrib
 mv opencv-3.4.4 opencv
 mv opencv_contrib-3.4.4 opencv_contrib
 
@@ -26,6 +28,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D INSTALL_C_EXAMPLES=OFF \
 	-D OPENCV_ENABLE_NONFREE=ON \
 	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D PYTHON_EXECUTABLE=$(which python3) \
 	-D BUILD_EXAMPLES=ON ..
 
 make -j$(nproc --all)
