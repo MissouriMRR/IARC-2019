ba2812e37914f1f960619f85b5f1a28647afb8cf
christopher-o-toole <cdocq9@mst.edu>
2019-03-08 22:45:32 -0600

    Added realsense install script

---
+++ flight/devices/scripts/install_realsense.sh
@@ -0,0 +1,4 @@
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo apt-get install librealsense2-dkms
+sudo apt-get install librealsense2-utils
