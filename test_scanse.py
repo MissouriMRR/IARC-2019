ca87476d5710e640cfa5967d892b39eef40dd246
Clay McGinnis <mcginnis@claymav.com>
2019-04-20 16:06:32 -0500

    Changes scanse obstacle avoidance strategy.

--- test_scanse.py
+++ test_scanse.py
@@ -15,6 +15,27 @@ REACT_DURATION = 0.2 # go in opposite direction for this many seconds
 LOG_LEVEL = logging.INFO
 SIGNAL_THRESHOLD = 170
 
+"""
+class React(threading.Thread):
+    def __init__(self, drone, sector):
+        super(React, self).__init__()
+        self.drone = drone
+        self.sector = sector
+        self.stop_event = threading.Event()
+
+    def run(self):
+        print(sector, sample.distance, sample.signal_strength)
+        start = timer()
+        duration = 0.2 # go in opposite direction for
+        direction = react_direction[sector]
+        x, y, z = direction
+        while timer() - start < duration:
+            #drone.send_velocity(x, y, z)
+            self.logger.info("Trying to move drone - {}, {}, {}".format(x, y, z))
+            time.sleep(1.0/MESSAGE_RESEND_RATE)
+        #self.flight_session.mode = Modes.NETWORK_CONTROLLED
+"""
+
 class Sectors(Enum):
     ONE = 1
     TWO = 2
@@ -57,53 +78,46 @@ class CollisionAvoidance(threading.Thread):
         """
         with Sweep(DEVICE) as sweep:
             sweep.set_motor_speed(6)
-            sweep.set_sample_rate(500)
+            sweep.set_sample_rate(100)
             sweep.start_scanning()
 
             for scan in sweep.get_scans():
                 if self.stop_event.is_set():
                     sweep.stop_scanning()
                     return
-                for i in range(len(scan.samples)):
-                    temp = scan.samples[i]
+                for temp in scan.samples:
                     if temp.signal_strength < SIGNAL_THRESHOLD:
                         continue
                     # ignore samples with distance one (they are faulty)
-                    if temp.distance == 1:
+                    if temp.distance == 1 or temp.distance > DETECTION_RANGE:
                         continue
+                    print(temp.distance, temp.signal_strength)
                     tempAngle = int(temp.angle/1000)
                     if tempAngle >= 0 and tempAngle < 45:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 1")
-                            self.react(Sectors.ONE, temp)
+                        self.logger.info("Collision detected - sector 1")
+                        self.react(Sectors.ONE, temp)
                     elif tempAngle >= 45 and tempAngle < 90:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 2")
-                            self.react(Sectors.TWO, temp)
+                        self.logger.info("Collision detected - sector 2")
+                        self.react(Sectors.TWO, temp)
                     elif tempAngle >= 90 and tempAngle < 135:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 3")
-                            self.react(Sectors.THREE, temp)
+                        self.logger.info("Collision detected - sector 3")
+                        self.react(Sectors.THREE, temp)
                     elif tempAngle >= 135 and tempAngle < 180:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 4")
-                            self.react(Sectors.FOUR, temp)
+                        self.logger.info("Collision detected - sector 4")
+                        self.react(Sectors.FOUR, temp)
                     elif tempAngle >= 180 and tempAngle < 225:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 5")
-                            self.react(Sectors.FIVE, temp)
+                        self.logger.info("Collision detected - sector 5")
+                        self.react(Sectors.FIVE, temp)
                     elif tempAngle >= 225 and tempAngle < 270:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 6")
-                            self.react(Sectors.SIX, temp)
+                        self.logger.info("Collision detected - sector 6")
+                        self.react(Sectors.SIX, temp)
                     elif tempAngle >= 270 and tempAngle < 315:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 7")
-                            self.react(Sectors.SEVEN, temp)
+                        self.logger.info("Collision detected - sector 7")
+                        self.react(Sectors.SEVEN, temp)
                     elif tempAngle >= 315 and tempAngle < 360:
-                        if temp.distance < DETECTION_RANGE:
-                            self.logger.info("Collision detected - sector 8")
-                            self.react(Sectors.EIGHT, temp)
+                        self.logger.info("Collision detected - sector 8")
+                        self.react(Sectors.EIGHT, temp)
+                    break
     def react(self, sector, sample):
         """
         Reacts to an obstacle detected in this sector by travelling
@@ -126,7 +140,7 @@ class CollisionAvoidance(threading.Thread):
         """
         print(sector, sample.distance, sample.signal_strength)
         start = timer()
-        duration = 0 # go in opposite direction for
+        duration = 0.2 # go in opposite direction for
         direction = react_direction[sector]
         x, y, z = direction
         while timer() - start < duration:
