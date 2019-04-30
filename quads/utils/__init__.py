d85701f48c6dc7449083707354f6fd59fa80e03e
Thomas McKanna <thomasmckanna@gmail.com>
2019-04-29 19:41:39 -0500

    Fixed run time error.

--- quads/utils/__init__.py
+++ quads/utils/__init__.py
@@ -1,9 +1,9 @@
-import commands
+from utils.commands import Takeoff, Move, Heal
 
-from collision_avoidance import CollisionAvoidance
-from drone import Drone
-from input_thread import InputThread
-from modes import Modes
-from netclient import NetClient
+from utils.collision_avoidance import CollisionAvoidance
+from utils.drone import Drone
+from utils.input_thread import InputThread
+from utils.modes import Modes
+from utils.netclient import NetClient
 
 from .utils import parse_command
